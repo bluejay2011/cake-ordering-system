6d5dba6c00c5657d0736685298136d77f747e076
Stephanie Jose <stephjose2011@gmail.com>
2020-11-28 23:07:20 +0800

    return create order details

--- orderManager.js
+++ orderManager.js
@@ -1,81 +1,48 @@
 'use strict'
 
-const uuidv1 = require('uuid/v1');
+const { v1: uuidv1 } = require('uuid');
 const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient();
 const kinesis = new AWS.Kinesis();
 
 const TABLE_NAME = process.env.orderTableName;
-const STREAM_NAME = process.env.orderStreamName;
+const STREAM_NAME = process.env.orderStreamName
 
 module.exports.createOrder = body => {
-  const order = {
-    orderId: uuidv1(),
-    name: body.name,
-    address: body.address,
-    productId: body.productId,
-    quantity: body.quantity,
-    orderDate: Date.now(),
-    eventType: 'order_place'
-  }
+    const order = {
+        orderId: uuidv1(),
+        name: body.name,
+        address: body.address,
+        productId: body.productId,
+        quantity: body.quantity,
+        orderDate: Date.now(),
+        eventType: 'order_placed'
+    }
 
-  return order;
+    return order;
 }
 
-module.exports.placeNewOrder = body => {
-  // save order in table
-  // put order in stream
-  return saveOrder(order).then(() => {
-    return placeOrderStream(order);
-  });
-}
-
-module.exports.fulfillOrder = (orderId, fulfillmentId)  => {
-
-  return getOrder(orderId).then(savedOrder => {
-    const order = createFulfilledOrder(savedOrder, fulfillmentId);
-    return saveOrder(order).then({
-      return placeOrderStream(order);
-    });
-  });
-}
-
-function saveOrder(order) {
-  const params = {
-    TableName: TABLE_NAME,
-    Item: order
-  }
-
-  return dynamo.put(param).promise();
+module.exports.placeNewOrder = order => {
+    return saveNewOrder(order).then(() => {
+        return placeOrderStream(order)
+    })
 }
 
 function placeOrderStream(order) {
-  const params = {
-    Data: JSON.stringify(order),
-    PartitionKey: order.orderId,
-    StreamName: STREAM_NAME
-  }
+    const params = {
+        Data: JSON.stringify(order),
+        PartitionKey: order.orderId,
+        StreamName: STREAM_NAME
+    }
 
-  return kinesis.putRecord(params).promise();
+    return kinesis.putRecord(params).promise();
 }
 
-function getOrder(orderId) {
-  const params = {
-    Key: {
-      orderId: orderId
-    },
-    TableName: TABLE_NAME,
-  }
+function saveNewOrder(order) {
+    const params = {
+        TableName: TABLE_NAME,
+        Item: order
+    }
 
-  return dynamo.get(params).promise().then(result => {
-    return result.Item;
-  });
-}
-
-function createFulfilledOrder(savedOrder, fulfillmentId) {
-  savedOrder.fulfillmentId = fulfillmentId;
-  savedOrder.fulfillmentDate = Date.now();
-  savedOrder.eventType = 'order_fulfilled';
-
-  return savedOrder;
+    return dynamo.put(params).promise()
 }
