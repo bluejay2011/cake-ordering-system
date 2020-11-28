08645d2b19cb61210b8333ca8e1e81bbb79621b3
Stephanie Jose <stephjose2011@gmail.com>
2020-11-28 23:06:07 +0800

    update handler.js

--- handler.js
+++ handler.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const orderManager = require('./orderManager');
+
 function createResponse(statusCode, message) {
   const response = {
     statusCode: statusCode,
     body: JSON.stringify(message)
-  }
+  };
 
   return response;
 }
@@ -15,29 +17,8 @@ module.exports.createOrder = async (event) => {
   const order = orderManager.createOrder(body);
 
   return orderManager.placeNewOrder(order).then(() => {
-      return createResponse(200, order);
+    return createResponse(200, order);
   }).catch(error => {
     return createResponse(400, error);
-  });
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Create order!',
-      input: event,
-    }),
-  };
-};
-
-module.exports.orderFulfillment = async (event) {
-  const body = JSON.parse(event.body);
-  const orderId = body.orderId;
-  const fulfillmentId = body.fulfillmentId;
-
-  return orderManager.fulfillOrder(orderId, fulfillmentId).then(() => {
-    return createResponse(200, 'Order with orderId:${orderId} was sent to deliver');
-  }).catch(error => {
-    return createResponse(400, error);
-  });
-
+  })
 };
